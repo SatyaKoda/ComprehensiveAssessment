2fdb3772311ab7b9456463c79c5eaf3babde267d
satya <Koda.VVSatyaSaiSuryaRamakrishna@mindtree.com>
2022-05-21 09:18:15 +0530

    commiting Bonvoy maven project

---
+++ Bonvoy/src/main/java/Hotel/Bonvoy/App.java
@@ -0,0 +1,13 @@
+package Hotel.Bonvoy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
